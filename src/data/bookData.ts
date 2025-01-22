d7d41274d6de334562a7b5327c854a93dbb1f36e
PreethiKalyani99 <preethikalyani99@gmail.com>
2025-01-22 22:08:09 +0530

    add pagination logic for books

--- src/data/bookData.ts
+++ src/data/bookData.ts
@@ -5,20 +5,20 @@ import { Authors } from "../entity/Authors";
 
 interface BookProps {
     author_id?: number;
-    published_year?: number; 
+    published_year?: number;
 }
 
-interface InsertProps extends BookProps{
+interface InsertProps extends BookProps {
     title: string
 }
 
-interface UpdateProps extends BookProps{
+interface UpdateProps extends BookProps {
     book_id: number
     title?: string
 }
 
-interface ListBooksProps {
-    book_id:  number | null
+interface GetBookById {
+    book_id: number | null
 }
 
 const books = AppDataSource.getRepository(Books)
@@ -35,7 +35,7 @@ export async function insertBook({ title, author_id, published_year }: InsertPro
     const newBook = new Books()
     newBook.title = title
 
-    if(author_id){
+    if (author_id) {
         const author = await isAuthorAvailable(author_id)
 
         if (!author) {
@@ -44,25 +44,26 @@ export async function insertBook({ title, author_id, published_year }: InsertPro
         newBook.author = author
     }
 
-    if(published_year){
+    if (published_year) {
         newBook.published_year = published_year
     }
 
     await books.save(newBook)
-}   
+    return newBook
+}
 
-export async function updateBook({ book_id, title, author_id, published_year }: UpdateProps){
-    const bookToUpdate = await books.findOne({ where: { book_id: book_id}})
+export async function updateBook({ book_id, title, author_id, published_year }: UpdateProps) {
+    const bookToUpdate = await books.findOne({ where: { book_id: book_id } })
 
-    if(!bookToUpdate){
+    if (!bookToUpdate) {
         throw new Error(`Book with id ${book_id} not found`)
     }
 
-    if(title){
+    if (title) {
         bookToUpdate.title = title
     }
 
-    if(author_id){
+    if (author_id) {
         const author = await isAuthorAvailable(author_id)
         if (!author) {
             throw new Error(`Author id ${author_id} not found`)
@@ -71,7 +72,7 @@ export async function updateBook({ book_id, title, author_id, published_year }: 
         bookToUpdate.author = author
     }
 
-    if(published_year){
+    if (published_year && !isNaN(published_year)) {
         bookToUpdate.published_year = published_year
     }
 
@@ -79,10 +80,10 @@ export async function updateBook({ book_id, title, author_id, published_year }: 
     return bookToUpdate
 }
 
-export async function deleteBook(book_id: number){
-    const bookToDelete = await books.findOne({ where: { book_id }})
+export async function deleteBook(book_id: number) {
+    const bookToDelete = await books.findOne({ where: { book_id } })
 
-    if(!bookToDelete){
+    if (!bookToDelete) {
         throw new Error(`Book with id ${book_id} not found`)
     }
     await books.remove(bookToDelete)
@@ -90,19 +91,35 @@ export async function deleteBook(book_id: number){
     return bookToDelete
 }
 
-export async function listBooks({ book_id }: ListBooksProps){
-    if(book_id){
-        const book = await books.findOne({ where: { book_id }, relations: ['author']})
-    
-        if(book){
-           return book
-        }
-        else{
-            throw new Error(`Book with id ${book_id} not found`)
+export async function getBookById({ book_id }: GetBookById) {
+    const book = await books.findOne({ where: { book_id }, relations: ['author'] })
+
+    if (book) {
+        return book
+    }
+    else {
+        throw new Error(`Book with id ${book_id} not found`)
+    }
+}
+
+export async function getBooksByPage({ page_number, page_size }){
+    const skip = (page_number - 1) * page_size
+    const [data, totalCount] = await books.findAndCount({ skip, take: page_size, relations:['author'] })
+
+    if (page_size > totalCount) {
+        const allData = await books.find({relations: ['author']})
+        return {
+            data: allData,
+            totalCount,
+            totalPages: Math.ceil(totalCount / page_size),
+            currentPage: page_number
         }
     }
-    else{
-        const allBooks = await books.find({relations: ['author']})
-        return allBooks
+
+    return {
+        data,
+        totalCount,
+        totalPages: Math.ceil(totalCount / page_size),
+        currentPage: page_number
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Express, Request, Response } from "express"
 import { AppDataSource } from "./data-source"
 import { insertAuthor, updateAuthor, deleteAuthor, getAuthorsById, getAuthorsByPage } from "./data/authorData"
-import { insertBook, updateBook, deleteBook, listBooks } from "./data/bookData"
+import { insertBook, updateBook, deleteBook, getBookById, getBooksByPage } from "./data/bookData"
 
 const app: Express = express()
 const port: number = 4000
@@ -109,7 +109,7 @@ AppDataSource.initialize().then(async () => {
                 throw new Error(`ID ${id} is invalid`);
             }
 
-            const result =  await listBooks({book_id: Number(id)})
+            const result =  await getBookById({book_id: Number(id)})
             res.status(200).json(result)
         }
         catch(error){
@@ -118,9 +118,78 @@ AppDataSource.initialize().then(async () => {
         }
     })
 
-    app.get('/books/all', async (req: Request, res: Response) => {
-        const result =  await listBooks({book_id: null})
-        res.status(200).json(result)
+    app.get('/books', async (req: Request, res: Response) => {
+        const { page_number, page_size } = req.query 
+        const pageNumber = /^[0-9]+$/.test(page_number?.toString()) ? Number(page_number) : 1
+        const pageSize = /^[0-9]+$/.test(page_size?.toString()) ? Number(page_size) : 10
+
+        try{
+            const result = await getBooksByPage({ page_number: pageNumber, page_size: pageSize})
+            res.status(200).json(result)
+        }
+        catch(error){
+            console.error('Error:', error.message)
+            res.status(404).json({ error: error.message })
+        }
+    })
+
+    app.post('/books', async (req: Request, res: Response) => {
+        const { author_id, title, published_year } = req.body
+        try{
+            if(!author_id || !title){
+                throw new Error('Author id and title are required')
+            }
+            if (author_id && !/^[0-9]+$/.test(author_id.toString())) {
+                throw new Error(`ID ${author_id} is invalid`);
+            }
+
+            const result = await insertBook({author_id: author_id, title: title, published_year: published_year})
+            res.status(201).json(result)
+
+        }
+        catch(error){
+            console.error('Error:', error.message)
+            res.status(404).json({ error: error.message })
+        }
+    })
+
+    app.put('/books/:id', async (req: Request, res: Response) => {
+        const { id } = req.params
+        const { author_id, title, published_year } = req.body
+
+        try{
+            if(!id){
+                throw new Error("ID is required")
+            }
+            if (id && !/^[0-9]+$/.test(id.toString())) {
+                throw new Error(`ID ${id} is invalid`);
+            }
+            const result = await updateBook({ book_id: Number(id), author_id: author_id, title: title, published_year: published_year })
+            res.status(200).json(result)
+        }
+        catch(error){
+            console.error('Error:', error.message)
+            res.status(404).json({ error: error.message })
+        }
+    })
+
+    app.delete('/books/:id', async (req: Request, res: Response) => {
+        const { id } = req.params
+
+        try{
+            if(!id){
+                throw new Error("ID is required")
+            }
+            if (id && !/^[0-9]+$/.test(id.toString())) {
+                throw new Error(`ID ${id} is invalid`)
+            }
+            await deleteBook(Number(id))
+            res.status(200).send()
+        }
+        catch(error){
+            console.error('Error:', error.message)
+            res.status(404).json({ error: error.message })
+        }
     })
     
     app.listen(port, () => {
